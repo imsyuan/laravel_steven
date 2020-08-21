fedbd711cfd9b155e6cfbeae9643a3ff88c266cd
steven.chang <steven.chang@104.com.tw>
2020-08-21 12:45:10 +0800

    project init

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Http\Middleware\CheckAge;
 
 Route::get('/', function () {
     return view('welcome');
-});
+});;
